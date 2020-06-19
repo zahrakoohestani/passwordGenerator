c8a912ce159f04b488dabbbddcd8446672075be2
zahrakoohestani <zahrakoohestani11@gmail.com>
2020-06-19 22:22:18 +0430

    last commit

--- passwordGenerator/generator/views.py
+++ passwordGenerator/generator/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+import random
 
 def home(request):
     return render(request, 'generator/home.html')
+
+def password(request):
+    characters=list('asdfghjklqwertyuiopzxcvbnm')
+    if request.GET.get('uppercase'):
+        characters.extend(list('ASDFGHJKLQWERTYUIOPZXCVBNM'))
+    if request.GET.get('symbols'):
+        characters.extend(list('!@#$%^&*()_+'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+    length=request.GET.get('length')
+    length=int(length)
+    thePassword=''
+    for i in range(length):
+        thePassword+=random.choice(characters)
+    return render(request, 'generator/password.html', {'password':thePassword})
--- passwordGenerator/passwordGenerator/urls.py
+++ passwordGenerator/passwordGenerator/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from generator import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('password', views.password, name='password'),
 ]
